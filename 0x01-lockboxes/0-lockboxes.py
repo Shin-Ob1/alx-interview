8b1af669be581901b06f1848840b139ddd6325fb
ShinOb1 <obikelv@gmail.com>
2024-01-11 05:14:24 -0800

    lockbox interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''LockBoxes Challenge: Alx interview'''
+
+
+def canUnlockAll(boxes):
+    '''checks if all the boxes can be opened or not
+    Returns:
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
